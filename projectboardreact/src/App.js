beb0f12696d9e0992a8d7c39c69c47b1d3e192ab
Abdi <abdi.noor0@gmail.com>
2019-05-22 05:25:53 -0500

    minor adjustments

--- projectboardreact/src/App.js
+++ projectboardreact/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css"
-import './App.css';
 import Navbar from './components/Navbar';
 import ProjectBoard from './components/ProjectBoard';
 
