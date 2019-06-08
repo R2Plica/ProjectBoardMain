92d1a55e903f7e12f2711948a5c34afd04b138e2
Abdi Noor <abdi.noor0@gmail.com>
2019-06-07 23:29:18 -0500

    Adding add/update components

--- projectboardreact/src/App.js
+++ projectboardreact/src/App.js
@@ -2,13 +2,20 @@ import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css"
 import Navbar from './components/Navbar';
 import ProjectBoard from './components/ProjectBoard';
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import AddProjectTask from './components/ProjectTask/AddProjectTask';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div className="App">
       <Navbar />
-      <ProjectBoard />
+      <Route exact path="/" component={ProjectBoard} />
+      <Route exact path="/addProjectTask" component={AddProjectTask} />
+      
     </div>
+    </Router>
+    
   );
 }
 
--- projectboardreact/src/components/ProjectBoard.js
+++ projectboardreact/src/components/ProjectBoard.js
@@ -1,41 +1,26 @@
 import React, { Component } from 'react'
-
+import { Link } from 'react-router-dom';
+import ProjectTaskItem from './ProjectTask/ProjectTaskItem';
  class ProjectBoard extends Component {
     render() {
         return (
             <div>
             <div className="container">
-            <a href="/ProjectTaskForm.html" className="btn btn-primary mb-3">
-                <i className="fas fa-plus-circle"> Create Project Task</i>
-            </a>
+            <Link to="/addProjectTask" className="btn btn-primary mb-3">
+                <i> Create Project Task</i>
+            </Link>
             <br />
             <hr />
             <div className="container">
                 <div className="row">
+                    
                     <div className="col-md-4">
                         <div className="card text-center mb-2">
                             <div className="card-header bg-secondary text-white">
                                 <h3>TO DO</h3>
                             </div>
                         </div>
-
-                        <div className="card mb-1 bg-light">
-                            <div className="card-header text-primary">
-                                ID: projectSequence
-                            </div>
-                            <div className="card-body bg-light">
-                                <h5 className="card-title">summary</h5>
-                                <p className="card-text text-truncate ">
-                                    acceptanceCriteria
-                                </p>
-                                <a href="#" className="btn btn-primary">
-                                    View / Update
-                                </a>
-                                <button className="btn btn-danger ml-4">
-                                    Delete
-                                </button>
-                            </div>
-                        </div>
+                    <ProjectTaskItem />
                     </div>
 
                     <div className="col-md-4">
